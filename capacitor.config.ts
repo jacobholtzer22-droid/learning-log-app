3e50798713df7c76eeb222c472b30926cf95f5eb
Jacob Holtzer <Jacobholtzer22@gmail.com>
2026-01-30 12:10:13 -0500

    Add Capacitor and iOS project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learninglog.app',
+  appName: 'learning-log-app',
+  webDir: 'public'
+};
+
+export default config;
